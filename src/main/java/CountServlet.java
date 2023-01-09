a71cd1c5ac4a9560c7af43ef6bd905d169f067d5
Brittney Brinson <brittneyc95@outlook.com>
2023-01-08 21:40:35 -0500

    Finished exercise

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -5,12 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello") //when a user visits this URL they will see what's in the body of the method.
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "CountServlet", urlPatterns = "/countpage") //I have /count already mapped somewhere
+public class CountServlet extends HttpServlet {
+    protected int x = 0;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        x += 1;
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();  //print statement for the browser instead of the console
-        out.println("<h1>Hello, World</h1>");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>This page was viewed: " + x + " times</h1>");
     }
+
 }
